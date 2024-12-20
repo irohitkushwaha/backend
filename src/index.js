80bf871e07eb185fd95e0ccf86a1014dc513456b
Rohit Kushwaha <rohitkushwaha.r780@gmail.com>
2024-12-20 09:10:14 +0530

    used then catch

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import dotenv from "dotenv";
 import DBConnection from "./db/index.js";
 
 dotenv.config();
-console.log("importing env", process.env.PORT)
+console.log("importing env", process.env.PORT);
+
 DBConnection()
+.then(()=>{
+    
+})
+.catch(); 

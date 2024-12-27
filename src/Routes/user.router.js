2626f292cd4873626d0f688362faf94acb3a5ed2
Rohit Kushwaha <rohitkushwaha.r780@gmail.com>
2024-12-27 09:04:23 +0530

    temporary for learning, file added

--- src/Routes/user.router.js
+++ src/Routes/user.router.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import upload from "../Middlewares/file.middleware.js";
 import RegisterUser from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 const router = Router();
 
@@ -15,5 +17,14 @@ const router = Router();
 
   RegisterUser
 );
+
+
+//next
+
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
 
 export default router;

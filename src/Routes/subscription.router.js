a224389b2a1b869116b9472c245bce7adef1aece
Rohit Kushwaha <rohitkushwaha.r780@gmail.com>
2025-01-06 11:37:07 +0530

    unsubscribed working successfully

--- src/Routes/subscription.router.js
+++ src/Routes/subscription.router.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import upload from "../Middlewares/file.middleware.js";
 import VerifyJWT from "../Middlewares/verifyjwt.middleware.js";
-import { SubscriberData } from "../controllers/subscription.controller.js";
+import {
+  SubscriberData,
+  SubscriptionStatus,
+  DeleteSubscriberData,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
 
-router.route("/:VideoId").post(VerifyJWT,SubscriberData)
+router.route("/:VideoId").post(VerifyJWT, SubscriberData);
 
-export default router
+router.route("/check/:VideoId").get(VerifyJWT, SubscriptionStatus);
+
+router.route("/unsubscribe/:VideoId").get(VerifyJWT, DeleteSubscriberData);
+
+export default router;
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -56,11 +56,102 @@ const SubscriberData = asyncHandler(async (req, res) => {
     Channel: SubscribedTo.Owner,
   });
 
-  res.status(200).json(new ApiResponse(SaveSubscription, 200, "Subscribed successfully"));
+  res
+    .status(200)
+    .json(new ApiResponse(SaveSubscription, 200, "Subscribed successfully"));
 });
 
-export {SubscriberData}
+//Algorithm to check whether isSubscribed or not
+//1. take the logged user id and video id using params
+//2. make sure its valid id
+//3. make sure each exist
+//4. check subscriptionStatus with user id and video owner id
+//5. send response to the user in boolean value
 
+const SubscriptionStatus = asyncHandler(async (req, res) => {
+  const userid = req.user._id;
+  const VideoId = req.params.VideoId;
 
+  if (!mongoose.Types.ObjectId.isValid(userid)) {
+    throw new ApiError(400, "User id is not valid");
+  }
 
+  if (!mongoose.Types.ObjectId.isValid(VideoId)) {
+    throw new ApiError(400, "Video id is not valid");
+  }
 
+  const UserID = await User.findById(userid);
+  if (!UserID) {
+    throw new ApiError(400, "User id doesn`t exist");
+  }
+
+  const VIDEOID = await Video.findById(VideoId);
+
+  if (!VIDEOID) {
+    throw new ApiError(400, "Video id doesn`t exist");
+  }
+
+  const SubscriptionExist = await Subscription.findOne({
+    Subscriber: UserID,
+    Channel: VIDEOID.Owner,
+  });
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        {SubscriptionExist, isSubscribed: !!SubscriptionExist },
+        200,
+        "Subscription status retrieved successfully"
+      )
+    );
+});
+
+//unsubscribed
+const DeleteSubscriberData = asyncHandler(async (req, res) => {
+  const SubscriberId = req.user._id;
+  const VideoId = req.params.VideoId;
+
+  if (!mongoose.Types.ObjectId.isValid(SubscriberId)) {
+    throw new ApiError(400, "Invalid subscriber ID");
+  }
+  if (!mongoose.Types.ObjectId.isValid(VideoId)) {
+    throw new ApiError(400, "Invalid video ID");
+  }
+
+  const Subscriber = await User.findById({
+    _id: SubscriberId,
+  });
+
+  const SubscribedTo = await Video.findOne({
+    _id: VideoId,
+  });
+
+  if (!Subscriber) {
+    throw new ApiError(400, "subscribing user doesn`t exist");
+  }
+
+  if (!SubscribedTo) {
+    throw new ApiError(400, "Video doesn`t exist");
+  }
+
+  const SubscriptionExist = await Subscription.findOne({
+    Subscriber: Subscriber._id,
+    Channel: SubscribedTo.Owner,
+  });
+
+  if (!SubscriptionExist) {
+    throw new ApiError(400, "User has not subscribed");
+  }
+
+  const SaveSubscription = await Subscription.deleteOne({
+    Subscriber: Subscriber._id,
+    Channel: SubscribedTo.Owner,
+  });
+
+  res
+    .status(200)
+    .json(new ApiResponse(SaveSubscription, 200, "UnSubscribed successfully"));
+});
+
+export { SubscriberData, SubscriptionStatus, DeleteSubscriberData };
